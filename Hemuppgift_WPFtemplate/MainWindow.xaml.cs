8ca2f0535d8119fd8089fa009294ce912336bda0
NanEkman <nen_92@hotmail.com>
2024-10-03 17:54:43 +0200

    Windows skapade för samtliga 4 buttons samt navigerat till de nya fönsterna i Code-behind

--- Hemuppgift_WPFtemplate/MainWindow.xaml.cs
+++ Hemuppgift_WPFtemplate/MainWindow.xaml.cs
@@ -15,22 +15,22 @@ namespace Hemuppgift_WPFtemplate
 
         private void GridButton(object sender, RoutedEventArgs e)
         {
-
+            MainFrame.Navigate(new Grid_Window());
         }
 
         private void StackPanelButton(object sender, RoutedEventArgs e)
         {
-
+            MainFrame.Navigate(new StackPanel_Window());
         }
 
         private void WrapPanelButton(object sender, RoutedEventArgs e)
         {
-
+            MainFrame.Navigate(new WrapPanel_Window());
         }
 
         private void DockPanelButton(object sender, RoutedEventArgs e)
         {
-
+            MainFrame.Navigate(new DockPanel_Window());
         }
     }
 }
