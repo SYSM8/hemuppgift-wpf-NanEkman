3d3a6029bd3c458eb555209c78aa1037a37ba97b
NanEkman <nen_92@hotmail.com>
2024-10-06 18:06:33 +0200

    skapade scrollviewer istället för scrollbar och ökade höjd på canvas i stackpanel

--- Hemuppgift_WPFtemplate/DockPanel Window.xaml.cs
+++ Hemuppgift_WPFtemplate/DockPanel Window.xaml.cs
@@ -23,5 +23,10 @@ namespace Hemuppgift_WPFtemplate
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
