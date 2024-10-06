a17527c9e52615bd0198920ef36f9584497a923b
NanEkman <nen_92@hotmail.com>
2024-10-06 15:29:08 +0200

    Fått mina radiobuttons till att funka, men har problem att lägga till scrollbar

--- Hemuppgift_WPFtemplate/StackPanel Window.xaml.cs
+++ Hemuppgift_WPFtemplate/StackPanel Window.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Shapes;
 
 namespace Hemuppgift_WPFtemplate
@@ -22,6 +23,78 @@ namespace Hemuppgift_WPFtemplate
         public StackPanel_Window()
         {
             InitializeComponent();
+        }
+
+        private void rbtn1_Click(object sender, RoutedEventArgs e)
+        {
+            if (rbtn1.IsChecked == true)
+            {
+                // Sätt alla rektanglar i horisontell layout
+                SetHorizontalLayout();              
+            }
+        }
+
+        private void rbtn2_Click(object sender, RoutedEventArgs e)
+        {
+            if(rbtn2.IsChecked == true)
+            {
+                // Sätt alla rektanglar i vertikal layout
+                SetVerticalLayout();
+            }
+        }
+
+        private void SetHorizontalLayout()
+        {
+            Canvas.SetLeft(rek1, 20);
+            Canvas.SetTop(rek1, 50);
+
+            Canvas.SetLeft(rek2, 130);
+            Canvas.SetTop(rek2, 50);
+
+            Canvas.SetLeft(rek3, 230);
+            Canvas.SetTop(rek3, 50);
+
+            Canvas.SetLeft(rek4, 320);
+            Canvas.SetTop(rek4, 50);
+
+            Canvas.SetLeft(rek5, 400);
+            Canvas.SetTop(rek5, 50);
+
+            Canvas.SetLeft(rek6, 470);
+            Canvas.SetTop(rek6, 50);
+
+            Canvas.SetLeft(rek7, 530);
+            Canvas.SetTop(rek7, 50);
+
+            Canvas.SetLeft(rek8, 580);
+            Canvas.SetTop(rek8, 50);
+        }
+
+        private void SetVerticalLayout()
+        {
+            Canvas.SetLeft(rek1, 50);
+            Canvas.SetTop(rek1, 20);
+
+            Canvas.SetLeft(rek2, 50);
+            Canvas.SetTop(rek2, 130);
+
+            Canvas.SetLeft(rek3, 50);
+            Canvas.SetTop(rek3, 230);
+
+            Canvas.SetLeft(rek4, 50);
+            Canvas.SetTop(rek4, 320);
+
+            Canvas.SetLeft(rek5, 50);
+            Canvas.SetTop(rek5, 400);
+
+            Canvas.SetLeft(rek6, 50);
+            Canvas.SetTop(rek6, 470);
+
+            Canvas.SetLeft(rek7, 50);
+            Canvas.SetTop(rek7, 530);
+
+            Canvas.SetLeft(rek8, 50);
+            Canvas.SetTop(rek8, 580);
         }
     }
 }
